679308c3d5271b4c18b875748e7e4b17faa7eceb
sujeong11 <87969121+sujeong11@users.noreply.github.com>
2025-04-30 14:41:40 +0900

    2025-04-30

---
+++ Lv. 1 /가장 가까운 같은 글자.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = []
+    
+    for idx, val in enumerate(s):
+        x = s[:idx]
+        find_idx = x.rfind(val)
+        if find_idx == -1:
+            answer.append(-1)
+        else:
+            answer.append(idx - find_idx)
+    
+    return answer
