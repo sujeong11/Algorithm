58e3d6e84b0b5431943bdaf282aa660a073ff9fc
sujeong11 <87969121+sujeong11@users.noreply.github.com>
2025-05-18 11:03:09 +0900

    2025-05-18

---
+++ Lv. 1 /소수 찾기.py
@@ -0,0 +1,15 @@
+def is_prime(num):
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def solution(n):
+    answer = 0
+  
+    for i in range(2, n+1):
+        if is_prime(i):
+            answer += 1
+          
+    return answer
