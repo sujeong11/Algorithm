a8ee5dda28c9e5bc1a37e3cb660acc2d25a5f312
sujeong11 <87969121+sujeong11@users.noreply.github.com>
2025-05-29 09:28:05 +0900

    2025-05-29

---
+++ Lv. 2/구명보트.py
@@ -0,0 +1,11 @@
+def solution(people, limit) :
+    answer, start_idx, end_idx = 0, 0, len(people) - 1
+    people.sort()
+
+    while start_idx < end_idx:
+        if people[end_idx] + people[start_idx] <= limit:
+            start_idx += 1
+            answer += 1
+        end_idx -= 1
+        
+    return len(people) - answer
