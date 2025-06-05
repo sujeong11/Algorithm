5e07b3e541a156f9cdea4b0933b11644d20b3a83
sujeong11 <87969121+sujeong11@users.noreply.github.com>
2025-06-05 13:47:02 +0900

    2025-06-05

---
+++ Lv. 2/타겟 넘버.py
@@ -0,0 +1,16 @@
+count = 0
+
+def dfs(numbers, target, current, idx):
+    global count
+    
+    if idx == len(numbers):
+        if target == current:
+            count += 1
+        return
+    
+    dfs(numbers, target, current + numbers[idx], idx + 1)
+    dfs(numbers, target, current - numbers[idx], idx + 1)
+
+def solution(numbers, target):
+    dfs(numbers, target, 0, 0)
+    return count
